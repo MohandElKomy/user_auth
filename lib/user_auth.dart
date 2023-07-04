0054c61f2ed5026feea5de69484c343cb1e8d11d
MohandElkomy <mohanadelkomy29@gmail.com>
2023-07-04 12:06:53 +0300

    Fix UI

--- lib/user_auth.dart
+++ lib/user_auth.dart
@@ -23,91 +23,92 @@ class UserLogin extends GetView<AuthController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GestureDetector(
-        onTap: () {
-          FocusScope.of(context).unfocus();
-        },
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: Expanded(
-            child: Form(
-              key: controller.form,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CustomTextFormField(
-                    controller: controller.userController,
-                    textPadding: 1,
-                    text: user == UserTypes.name
-                        ? 'User Name'
-                        : user == UserTypes.mail
-                            ? 'User Mail'
-                            : 'User Phone',
-                    hint: user == UserTypes.name
-                        ? 'Write Your Name'
-                        : user == UserTypes.mail
-                            ? 'Write Your Mail'
-                            : 'Write User Phone',
-                    borderRadius: 15,
-                    borderColor: AppColors.borderColor,
-                    isFilled: true,
-                    submit: () {},
-                    textInputAction: TextInputAction.next,
-                    validator: (value) {
-                      return AppValid.validName(value!);
-                    },
-                  ),
-                  user == UserTypes.phone
-                      ? const SizedBox()
-                      : Column(
-                          children: [
-                            const SizedBox(height: 15),
-                            CustomTextFormField(
-                              controller: controller.userController,
-                              textPadding: 1,
-                              text: 'password',
-                              hint: 'write your password',
-                              borderRadius: 15,
-                              borderColor: AppColors.borderColor,
-                              isFilled: true,
-                              submit: () {},
-                              textInputAction: TextInputAction.next,
-                              validator: (value) {
-                                if (user == UserTypes.name) {
-                                  return AppValid.validName(value!);
-                                } else if (user == UserTypes.phone) {
-                                  return AppValid.validPhone(value!);
-                                } else if (user == UserTypes.mail) {
-                                  return AppValid.validEmail(value!);
-                                }
-                                return null;
-                              },
-                            ),
-                          ],
-                        ),
-                  const SizedBox(height: 40),
-                  Obx(
-                    () => CustomProgressButton(
-                      value: 'Sign in'.tr,
-                      activeColor: AppColors.black,
-                      isLoading: loading.value,
-                      height: 50,
-                      border: 40,
-                      textStyle: (_) => const TextStyle(
-                        fontSize: 20,
-                        fontFamily: 'ReadexPro',
-                        color: AppColors.white,
-                      ),
-                      onPressed: loading.value ? () {} : onPress,
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Expanded(
+          child: Form(
+            key: controller.form,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CustomTextFormField(
+                  controller: controller.userController,
+                  textPadding: 1,
+                  text: user == UserTypes.name
+                      ? 'User Name'
+                      : user == UserTypes.mail
+                      ? 'User Mail'
+                      : 'User Phone',
+                  hint: user == UserTypes.name
+                      ? 'Write Your Name'
+                      : user == UserTypes.mail
+                      ? 'Write Your Mail'
+                      : 'Write User Phone',
+                  borderRadius: 15,
+                  borderColor: AppColors.borderColor,
+                  isFilled: true,
+                  submit: () {},
+                  textInputAction: TextInputAction.next,
+                  validator: (value) {
+                    return AppValid.validName(value!);
+                  },
+                ),
+                user == UserTypes.phone
+                    ? const SizedBox()
+                    : Column(
+                  children: [
+                    const SizedBox(height: 15),
+                    CustomTextFormField(
+                      controller: controller.userController,
+                      textPadding: 1,
+                      text: 'password',
+                      hint: 'write your password',
+                      borderRadius: 15,
+                      borderColor: AppColors.borderColor,
+                      isFilled: true,
+                      submit: () {},
+                      textInputAction: TextInputAction.next,
+                      validator: (value) {
+                        if (user == UserTypes.name) {
+                          return AppValid.validName(value!);
+                        } else if (user == UserTypes.phone) {
+                          return AppValid.validPhone(value!);
+                        } else if (user == UserTypes.mail) {
+                          return AppValid.validEmail(value!);
+                        }
+                        return null;
+                      },
                     ),
-                  ),
-                ],
-              ),
+                  ],
+                ),
+                const SizedBox(height: 40),
+                Obx(
+                      () =>
+                      CustomProgressButton(
+                        value: 'Sign in'.tr,
+                        activeColor: AppColors.black,
+                        isLoading: loading.value,
+                        height: 50,
+                        border: 40,
+                        textStyle: (_) =>
+                        const TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'ReadexPro',
+                          color: AppColors.white,
+                        ),
+                        onPressed: loading.value ? () {} : onPress,
+                      ),
+                ),
+              ],
             ),
           ),
         ),
       ),
+
     );
   }
 }
